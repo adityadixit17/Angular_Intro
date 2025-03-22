5b26b1a0529f964c557ac7d17f222890ceb4df1b
aditya dixit <adirkt2003@gmail.com>
2025-03-22 14:37:52 +0530

    Added

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'From Bridgelabz';
+  message = 'Hello From Bridgelabz';
 }
